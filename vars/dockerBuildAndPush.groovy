a0451e1a6b9338431dfc603fa0f2a92428b01908
madhubuddy28 <70298238+madhubuddy28@users.noreply.github.com>
2025-07-30 01:13:19 +0530

    Update dockerBuildAndPush.groovy

--- vars/dockerBuildAndPush.groovy
+++ vars/dockerBuildAndPush.groovy
@@ -1,1 +1,11 @@
+def call(Map config) {
+  def imageTag = "${config.acrName}.azurecr.io/${config.imageName}:${config.tag}"
 
+  sh """
+    echo '🐳 Building Docker image: ${imageTag}'
+    docker build -t ${imageTag} .
+
+    echo '📤 Pushing Docker image to ACR...'
+    docker push ${imageTag}
+  """
+}
